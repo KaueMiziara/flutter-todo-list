e70a1f0cd87b25e49cd3a7a727997dffe6d4f9a4
Kaue Miziara <kauemiziara@gmail.com>
2024-01-19 11:33:53 -0300

    feat: create placeholder list to TaskBox widget

--- lib/components/sections/task_box.dart
+++ lib/components/sections/task_box.dart
@@ -5,6 +5,33 @@ class TaskBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Items list");
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(28, 32, 28, 8),
+      child: Container(
+        height: 200,
+        color: Colors.black12,
+        child: const SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("Item 1"),
+              Divider(),
+              Text("Item 2"),
+              Divider(),
+              Text("Item 3"),
+              Divider(),
+              Text("Item 4"),
+              Divider(),
+              Text("Item 5"),
+              Divider(),
+              Text("Item 6"),
+              Divider(),
+              Text("Item 7"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
