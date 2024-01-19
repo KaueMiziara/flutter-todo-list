71a5fc21f1a232b01d47132609565c08f4a05904
Kaue Miziara <kauemiziara@gmail.com>
2024-01-19 19:32:26 -0300

    feat: add placeholder forms fields to NewTaskScreen widget

--- lib/screens/new_task_screen.dart
+++ lib/screens/new_task_screen.dart
@@ -10,7 +10,87 @@ class NewTaskScreen extends StatelessWidget {
     return const Scaffold(
       appBar: ScreenHeader(screenTitle: "New Task"),
       backgroundColor: TodoColors.gray,
-      body: Text("Hello, world!"),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Name",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Task Name'),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Category",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 24.0),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.circle, color: Colors.red),
+                        Icon(Icons.circle, color: Colors.green),
+                        Icon(Icons.circle, color: Colors.blue),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Date",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Date placeholder")
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Time",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text("Time placeholder")
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Description",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Description'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
