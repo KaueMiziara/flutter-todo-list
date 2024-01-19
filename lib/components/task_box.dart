622eace5ecbe174a90711a8d49fb5708245edd93
Kaue Miziara <kauemiziara@gmail.com>
2024-01-19 10:52:11 -0300

    Create TaskBox widget (WIP)

---
+++ lib/components/task_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskBox extends StatelessWidget {
+  const TaskBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Items list");
+  }
+}
