07ebdb4957061b6c1a798689808a1acd0793d564
Kaue Miziara <kauemiziara@gmail.com>
2024-01-25 21:00:41 -0300

    ui: wrap NewTaskPage's children in Padding and SingleChildScrollView to achieve desired positioning and spacing

--- lib/presentation/pages/new_task_page.dart
+++ lib/presentation/pages/new_task_page.dart
@@ -10,86 +10,97 @@ class NewTaskPage extends StatelessWidget {
     return const Scaffold(
       appBar: PageHeader(screenTitle: "New Task"),
       backgroundColor: TodoColors.gray,
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Name",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Task Name'),
-                ),
-              ],
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Name",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Task Name'),
+                  ),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Category",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 24.0),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 16.0),
-                    child: Row(
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Category",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 24.0),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.red),
+                          Icon(Icons.circle, color: Colors.green),
+                          Icon(Icons.circle, color: Colors.blue),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Icon(Icons.circle, color: Colors.red),
-                        Icon(Icons.circle, color: Colors.green),
-                        Icon(Icons.circle, color: Colors.blue),
+                        Text(
+                          "Date",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                        Text("Date placeholder")
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Date",
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                      ),
-                      Text("Date placeholder")
-                    ],
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Time",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                        Text("Time placeholder")
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Time",
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                      ),
-                      Text("Time placeholder")
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Description",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Description'),
-                ),
-              ],
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Description",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Description'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
