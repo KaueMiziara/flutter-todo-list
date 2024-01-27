ddede0e5214122c9d8d2fe06d5a3aa562629292a
Kaue Miziara <kauemiziara@gmail.com>
2024-01-27 18:48:54 -0300

    feat: create TaskTextInput widget to avoid coding repetition

---
+++ lib/presentation/components/elements/task_page/task_text_input.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/presentation/themes/todo_colors.dart';
+
+class TaskTextInput extends StatelessWidget {
+  const TaskTextInput(
+      {super.key, required this.title, required this.labelText});
+
+  final String title;
+  final String labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: labelText,
+            filled: true,
+            fillColor: Colors.white,
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: TodoColors.blue)),
+          ),
+        ),
+      ],
+    );
+  }
+}
