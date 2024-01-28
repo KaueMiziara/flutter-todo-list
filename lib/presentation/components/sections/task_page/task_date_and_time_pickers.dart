8a25d39245617903b2f8e45d28f9b7ff63849d7b
Kaue Miziara <kauemiziara@gmail.com>
2024-01-28 16:02:48 -0300

    refactor: extract TaskAndDateTimePickers's Time picker to the new TaskTimePicker widget

--- lib/presentation/components/sections/task_page/task_date_and_time_pickers.dart
+++ lib/presentation/components/sections/task_page/task_date_and_time_pickers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/presentation/components/elements/task_date_picker.dart';
-import 'package:todo_list/presentation/themes/todo_colors.dart';
+import 'package:todo_list/presentation/components/elements/task_time_picker.dart';
 
 class TaskDateAndTimePickers extends StatefulWidget {
   const TaskDateAndTimePickers({super.key});
@@ -10,61 +10,20 @@ class TaskDateAndTimePickers extends StatefulWidget {
 }
 
 class _TaskDateAndTimePickersState extends State<TaskDateAndTimePickers> {
-  final TextEditingController _timeController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
       child: Row(
         children: [
-          const Expanded(
+          Expanded(
             child: TaskDatePicker(),
           ),
           Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Time",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                TextField(
-                  controller: _timeController,
-                  decoration: const InputDecoration(
-                    labelText: "Time",
-                    filled: true,
-                    fillColor: Colors.white,
-                    suffixIcon: Icon(Icons.schedule),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: TodoColors.blue)),
-                  ),
-                  readOnly: true,
-                  onTap: () {
-                    _selectTime();
-                  },
-                ),
-              ],
-            ),
+            child: TaskTimePicker(),
           ),
         ],
       ),
     );
-  }
-
-  void _selectTime() async {
-    TimeOfDay? selectedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    );
-
-    if (selectedTime != null) {
-      setState(() {
-        _timeController.text = selectedTime.format(context).toString();
-      });
-    }
   }
 }
