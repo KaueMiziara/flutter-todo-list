ce2a00adc9b905b5232edfa4029ed75b0f6fe16f
Kaue Miziara <kauemiziara@gmail.com>
2024-01-29 16:33:10 -0300

    refactor: change TaskDateAndTimePickers to be a StatelessWidget

--- lib/presentation/components/sections/task_page/task_date_and_time_pickers.dart
+++ lib/presentation/components/sections/task_page/task_date_and_time_pickers.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:todo_list/presentation/components/elements/task_date_picker.dart';
 import 'package:todo_list/presentation/components/elements/task_time_picker.dart';
 
-class TaskDateAndTimePickers extends StatefulWidget {
+class TaskDateAndTimePickers extends StatelessWidget {
   const TaskDateAndTimePickers({super.key});
 
-  @override
-  State<TaskDateAndTimePickers> createState() => _TaskDateAndTimePickersState();
-}
-
-class _TaskDateAndTimePickersState extends State<TaskDateAndTimePickers> {
   @override
   Widget build(BuildContext context) {
     return const Padding(
