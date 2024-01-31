323892a951904fef4fdf24bf8b5d66c1245dbf42
Kaue Miziara <kauemiziara@gmail.com>
2024-01-31 18:54:04 -0300

    feat: add USER_ID string in .env file && configure flutter_dotenv dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/presentation/pages/home_page.dart';
 import 'package:todo_list/presentation/view_models/tasks_view_model.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load();
+
   runApp(ChangeNotifierProvider(
     create: (context) => TasksViewModel(),
     child: const TodoListApp(),
