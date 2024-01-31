d97178186c5812df6cb1810b246a527ae08df825
Kaue Miziara <kauemiziara@gmail.com>
2024-01-30 21:02:02 -0300

    refactor: remove TaskViewModel's unnecessary methods

--- lib/presentation/view_models/tasks_view_model.dart
+++ lib/presentation/view_models/tasks_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:todo_list/models/task.dart';
-import 'package:todo_list/presentation/components/elements/task_card.dart';
 
 class TasksViewModel extends ChangeNotifier {
   final List<Task> _tasks = [];
@@ -24,30 +23,5 @@ class TasksViewModel extends ChangeNotifier {
 
     _tasks.add(task);
     notifyListeners();
-  }
-
-  TasksViewModel() {
-    _populateList();
-  }
-
-  void _populateList() {
-    _tasks.add(Task(
-      id: "id1",
-      title: "title1",
-      date: DateTime(1),
-      category: "category",
-      description: "description",
-      completed: false,
-    ));
-  }
-
-  List<TaskCard> getCards() {
-    List<TaskCard> cardsList = [];
-
-    for (var task in _tasks) {
-      cardsList.add(TaskCard(task: task));
-    }
-
-    return cardsList;
   }
 }
