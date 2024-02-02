37b46095e6ffc8f8a5465a7f8a28420932194bcb
Kaue Miziara <kauemiziara@gmail.com>
2024-02-01 21:14:45 -0300

    refactor: change CreateTaskDto fields to final && update constructor

--- lib/dtos/create_task_dto.dart
+++ lib/dtos/create_task_dto.dart
@@ -2,19 +2,18 @@ import 'package:todo_list/models/task.dart';
 import 'package:todo_list/models/task_category.dart';
 
 class CreateTaskDto {
-  late String title;
-  late String description;
-  late String date;
-  late String category;
+  final String title;
+  final String description;
+  final String date;
+  final String category;
 
-  CreateTaskDto(Task task) {
-    title = task.title;
-    description = task.description;
-    date = task.date;
-    category = switch (task.category) {
-      TaskCategory.task => "TASK",
-      TaskCategory.event => "EVENT",
-      TaskCategory.achievement => "ACHIEVEMENT",
-    };
-  }
+  CreateTaskDto(Task task)
+      : title = task.title,
+        description = task.description,
+        date = task.date,
+        category = switch (task.category) {
+          TaskCategory.task => "TASK",
+          TaskCategory.event => "EVENT",
+          TaskCategory.achievement => "ACHIEVEMENT",
+        };
 }
