fdf87465e364564e450f565c8e395643fad89b01
Kaue Miziara <kauemiziara@gmail.com>
2024-02-02 20:24:46 -0300

    refactor: update CreateTaskDto's constructor to use TaskCategory's name getter instead of switch

--- lib/dtos/create_task_dto.dart
+++ lib/dtos/create_task_dto.dart
@@ -1,5 +1,4 @@
 import 'package:todo_list/models/task.dart';
-import 'package:todo_list/models/task_category.dart';
 
 class CreateTaskDto {
   final String title;
@@ -11,9 +10,5 @@ class CreateTaskDto {
       : title = task.title,
         description = task.description,
         date = task.date,
-        category = switch (task.category) {
-          TaskCategory.task => "TASK",
-          TaskCategory.event => "EVENT",
-          TaskCategory.achievement => "ACHIEVEMENT",
-        };
+        category = task.category.name;
 }
